727715afbed291636625b69860a060112ef04bef
Juna Salviati <juna.salviati@gmail.com>
2021-08-14 17:27:39 +0200

    TwoWay binding on ToDoItemAddPage added

--- ToDoApp/ToDoItemAddPage.xaml.cs
+++ ToDoApp/ToDoItemAddPage.xaml.cs
@@ -1,16 +1,31 @@
 ﻿using System;
+using System.Diagnostics;
 using Microsoft.Maui.Controls;
+using ToDoApp.Models;
 using ToDoApp.ViewModels;
 
 namespace ToDoApp
 {
 	public partial class ToDoItemAddPage : ContentPage
 	{
+
+		ToDoItemViewModel NewToDoItem;
 		
 		public ToDoItemAddPage()
         {
 			InitializeComponent();
+			ToDoItem NewItem = new ToDoItem() { Title = "", Completed = false } ;
+			NewToDoItem = new ToDoItemViewModel(NewItem);
+			BindingContext = NewToDoItem;
+		}
+
+		private async void AddToDoItem(object sender, EventArgs args)
+		{
 			
+			//string toDoText = ToDoText.Text;
+			//string toDoCompleted = ToDoCompleted.SelectedItem.ToString();
+			Debug.WriteLine(@"\toDoText: "+ NewToDoItem.Title);
+			Debug.WriteLine(@"\toDoCompleted: " + NewToDoItem.Completed);
 		}
 
 
